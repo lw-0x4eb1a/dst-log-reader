f95b9d525324339723f6f9e60723cb93c5b68c0b
老王天天写bug <wzh2021@pku.edu.cn>
2025-03-08 01:05:31 +0800

    update download url

--- page/main.js
+++ page/main.js
@@ -7,10 +7,10 @@ if (location.hash === '#log-not-found') {
 // setup download links
 const download = Array.from(document.getElementById('download').getElementsByTagName('div'));
 download[0]/* windows */.addEventListener("click", function() {
-  let url = "https://www.baidu.com";
+  let url = "https://fs-im-kefu.7moor-fs1.com/ly/4d2c3f00-7d4c-11e5-af15-41bf63ae4ea0/1741366794351/log-reader.exe.zip";
   window.open(url);
 })
 download[1]/* macos */.addEventListener("click", function() {
-  let url = "https://www.bing.com";
+  let url = "https://fs-im-kefu.7moor-fs1.com/ly/4d2c3f00-7d4c-11e5-af15-41bf63ae4ea0/1741366794172/Log Reader_0.1.0_universal.dmg.zip";
   window.open(url);
 })
